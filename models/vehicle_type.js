62f3dfce201ba6b6a2fc7fd050af72458ef07185
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-10 01:32:37 +0530

    First commit

---
+++ models/vehicle_type.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
+
+//Test DB
+sequelize
+	.authenticate()
+	.then(() => console.log('Database connected...'))
+	.catch((err) => console.log(`Error: ${err}`));
+
+// sequelize model for vehicle_type table
+const VehicleType = sequelize.define(
+	'vehicle_type_lookup',
+	{
+		id: {
+			type: Sequelize.UUIDV4,
+			primaryKey: true
+		},
+		vehicle_type: {
+			type: Sequelize.STRING(45),
+			allowNull: false
+		}
+	},
+	{
+		timestamps: true,
+		createdAt: 'created_at',
+		updatedAt: 'modified_at',
+		freezeTableName: true
+	}
+);
+
+module.exports = VehicleType;
