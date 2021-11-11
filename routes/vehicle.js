4efb9b4dea66621c0ba8c1c6d0bda6a70a3a4046
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-11 12:51:34 +0530

    Modified

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -9,27 +9,20 @@ router.get('/', (req, res, next) => {
 // Get vehicle Type list
 router.get('/types', (req, res) =>
 	VehicleType.findAll()
-		.then((vehicle_types) => {
-			res.send(vehicle_types);
-			console.log(vehicle_types);
-
-			//res.sendStatus(200);
-		})
+		.then((vehicle_types) => res.send(vehicle_types))
 		.catch((err) => console.log(err))
 );
 
-//need for future purpose
+// Add a vehicle type
+router.post('/addtype', (req, res) => {
+	var datetime = new Date();
+	let created_at = datetime.now;
+	let modified_at = datetime.now;
+	let { id, vehicle_type } = req.body;
 
-// // Add a vehicle type
-// router.get('/addtype', (req, res) => {
-// 	// //var datetime = new Date();
-// 	// let created_at = datetime.now;
-// 	// let modified_at = datetime.now;
-// 	let { id,vehicle_type} = req.body;
-	
-// 	VehicleType.create(req.body )
-// 		.then((type) => res.redirect('/vehicle/types'))
-// 		.catch((err) => console.log(err));
-// });
+	VehicleType.create({ id, vehicle_type, created_at, modified_at })
+		.then((type) => res.redirect('/vehicle/types'))
+		.catch((err) => console.log(err));
+});
 
 module.exports = router;
