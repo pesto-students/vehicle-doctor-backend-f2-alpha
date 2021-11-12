5b1070938ea36e1771d2b78ea43de4bb33dbd7bb
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-13 02:15:20 +0530

    Added customer routes

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const vehicleRouter = require('./routes/vehicle');
 const serviceRouter = require('./routes/service');
-const dealerRouter = require('./routes/dealer')
+const dealerRouter = require('./routes/dealer');
+const customerRouter = require('./routes/customer');
 
 const port = process.env.port || 3001;
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,6 +21,7 @@ app.use('/', indexRouter);
 app.use('/vehicle', vehicleRouter);
 app.use('/service', serviceRouter);
 app.use('/dealer', dealerRouter);
+app.use('/customer', customerRouter);
 
 app.listen(port, () => {
 	console.log('Deployment Project - Listening on port 3001');
