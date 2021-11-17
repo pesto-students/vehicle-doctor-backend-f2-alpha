06005dec6a911064606cf82e50948bc4059016a1
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-17 22:37:36 +0530

    Modified to fetch and add Service History

--- routes/customer.js
+++ routes/customer.js
@@ -9,9 +9,11 @@ router.get('/', (req, res, next) => {
 // Retrieve customer details with Customer Id
 router.get('/:id', customers.getCustomerById);
 
+// Fetch customer service history using Customer Id
+router.get('/service/feedback/:id', customers.getServiceHistory);
+
 // Add a customer
 router.post('/add', customers.createCustomer);
-
 
 // Add service feedback
 router.post('/service/feedback', customers.addServiceFeedback);
