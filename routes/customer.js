cd680c7807e2cdc2936d7fc2d00af4e5b8189249
Gagandeep Singh <gagandeepsingh516@ymail.com>
2021-11-26 20:10:28 +0530

    code cleaning

--- routes/customer.js
+++ routes/customer.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const customers = require('../controllers/customer.controller');
 
-router.get('/', (req, res, next) => {
-	res.send('All the customer details');
-});
 
 // Retrieve customer details with Customer Id
 router.get('/:id', customers.getCustomerById);
