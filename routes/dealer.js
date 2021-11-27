67153ee25038e64924f4df4ee1bff7f18aba962b
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-27 13:03:33 +0530

    Added route to check Dealer login

--- routes/dealer.js
+++ routes/dealer.js
@@ -3,15 +3,17 @@ const router = express.Router();
 
 const dealerController = require('../controllers/dealercontroller');
 
-router.get('/dealerByID/:id/:serviceID',dealerController.getDealerbyID);
+router.get('/dealerByID/:id/:serviceID', dealerController.getDealerbyID);
 
-router.get('/serviceType/:serviceType',dealerController.getDealerbyServiceType);
+router.get('/serviceType/:serviceType', dealerController.getDealerbyServiceType);
 
-router.get('/dealersByCity/:city/:vehicleid',dealerController.getDealersByCity);
+router.get('/dealersByCity/:city/:vehicleid', dealerController.getDealersByCity);
 
-router.get('/serviceByDealerID/:id',dealerController.getServiceByDealerID);
+router.get('/serviceByDealerID/:id', dealerController.getServiceByDealerID);
 
-router.post('/addDealer',dealerController.AddDealer)
+router.post('/addDealer', dealerController.AddDealer);
+
+router.post('/checkLogin', dealerController.checkDealerCredentials);
 
 // router.get('/test', (req, res, next) => {
 // 	res.send('All the service details');
