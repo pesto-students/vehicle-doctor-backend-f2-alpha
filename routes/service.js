aa85bfc55b7c79b976b488b4fdc7ca74b4d7c164
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-27 20:03:04 +0530

    Modified to get all service types

--- routes/service.js
+++ routes/service.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const Status = require('../models/service_status')
-const serviceTypes = require('../models/service_types')
+const Status = require('../models/service_status');
+const serviceTypes = require('../models/service_types');
 
 router.get('/', (req, res, next) => {
 	res.send('All the service details');
 });
 
-router.get('/types/:service_type', (req, res, next) => serviceTypes.findAll(
-		   {
-	       where :{service_type: req.params.service_type},
-		  })
-			.then(serviceTypes=>
-			res.send(serviceTypes))
-			.catch(err => res.send(err)));
+router.get('/types', (req, res) =>
+	serviceTypes
+		.findAll({
+			attributes: ['id', 'service_type', 'service_name']
+		})
+		.then((serviceTypes) => res.send(serviceTypes))
+		.catch((err) => res.send(err))
+);
 
-router.get('/status', (req, res, next) => Status.findAll()
-			.then(status=>
-			res.send(status))
-			.catch(err => res.send(err)));
+router.get('/types/:service_type', (req, res, next) =>
+	serviceTypes
+		.findAll({
+			where: { service_type: req.params.service_type }
+		})
+		.then((serviceTypes) => res.send(serviceTypes))
+		.catch((err) => res.send(err))
+);
+
+router.get('/status', (req, res, next) =>
+	Status.findAll()
+		.then((status) => res.send(status))
+		.catch((err) => res.send(err))
+);
 
 module.exports = router;
