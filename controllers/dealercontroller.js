2487810180da3fe4e3e6eaa5eaee6ae957783016
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-27 13:03:07 +0530

    Added Dealer login check on email and password

--- controllers/dealercontroller.js
+++ controllers/dealercontroller.js
@@ -1,166 +1,181 @@
 const dealerModel = require('../models/dealerModel');
-const dealerServices = require("../models/dealer_serviceModel");
+const dealerServices = require('../models/dealer_serviceModel');
 const vehicleModel = require('../models/vehicle_type');
 const serviceModel = require('../models/service_history');
-const serviceTypes = require('../models/service_types')
+const serviceTypes = require('../models/service_types');
 const { Sequelize } = require('sequelize');
 
-
 exports.getDealerbyID = async (req, res, next) => {
-    try {
-    var dealerID = req.params.id;
-    var serviceID = req.params.serviceID;
-    let dealerData = await dealerModel.findOne(
-	{
-        attributes:['name','mobile','gst_no','locality','city','state','pincode'],
-        where :{dealer_id: dealerID},
-        include:[
-             {
-               model:vehicleModel,
-               as :'Vehicletype',
-               attributes: ['vehicle_type']
-             },
-            {  
-              model:dealerServices,
-              as :'Services' ,
-              attributes:['discription','cost'],
-              where:{service_type_id:serviceID}
-            },
-            {
-              model:serviceModel,
-              as:'dealer_history',
-              attributes:['rating','comments'],
-              order: [["created_at", "DESC"]],
-              limit:3
-            }
-        ]
-	})
-	res.json(dealerData);
-    } catch (err) {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    }
-  };
+	try {
+		var dealerID = req.params.id;
+		var serviceID = req.params.serviceID;
+		let dealerData = await dealerModel.findOne({
+			attributes: ['name', 'mobile', 'gst_no', 'locality', 'city', 'state', 'pincode'],
+			where: { dealer_id: dealerID },
+			include: [
+				{
+					model: vehicleModel,
+					as: 'Vehicletype',
+					attributes: ['vehicle_type']
+				},
+				{
+					model: dealerServices,
+					as: 'Services',
+					attributes: ['discription', 'cost'],
+					where: { service_type_id: serviceID }
+				},
+				{
+					model: serviceModel,
+					as: 'dealer_history',
+					attributes: ['rating', 'comments'],
+					order: [['created_at', 'DESC']],
+					limit: 3
+				}
+			]
+		});
+		res.json(dealerData);
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
 
 exports.getDealerbyServiceType = async (req, res, next) => {
-   try{
-        var serviceType = req.params.serviceType;
-        let dealersResult = await dealerModel.findAll(
-          
-            {
-          
-            include:[
-            {
-              model:dealerServices,
-              as :'Services',
-              where :{service_type_id: serviceType},
-            }, 
-            {
-              model:vehicleModel,
-              as :'Vehicletype',
-              attributes: ['vehicle_type']
-            },
-            {
-              model:serviceModel,
-              as:'dealer_history',
-              attributes:['rating','comments'],
-              order: [["created_at", "DESC"]],
-              limit:3
-            }
-          ],
-          })
-            res.json(dealersResult)
-     }
-     catch (err) {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    }
-  };
+	try {
+		var serviceType = req.params.serviceType;
+		let dealersResult = await dealerModel.findAll({
+			include: [
+				{
+					model: dealerServices,
+					as: 'Services',
+					where: { service_type_id: serviceType }
+				},
+				{
+					model: vehicleModel,
+					as: 'Vehicletype',
+					attributes: ['vehicle_type']
+				},
+				{
+					model: serviceModel,
+					as: 'dealer_history',
+					attributes: ['rating', 'comments'],
+					order: [['created_at', 'DESC']],
+					limit: 3
+				}
+			]
+		});
+		res.json(dealersResult);
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
 
-  exports.getDealersByCity = async (req, res, next) => {
-    try{
-        var cityValue = req.params.city;
-        var vehicleId = req.params.vehicleid;
+exports.getDealersByCity = async (req, res, next) => {
+	try {
+		var cityValue = req.params.city;
+		var vehicleId = req.params.vehicleid;
 
-          let dealersResult = await dealerModel.findAll(
-             {
-              where :{city: cityValue},
-              include:[
-                {
-                  model:vehicleModel,
-                  as :'Vehicletype',
-                  attributes: ['vehicle_type'],
-                  where:{id:vehicleId}
-                },
-               {  
-                 model:dealerServices,
-                 as :'Services' ,
-                 attributes:['discription','cost']
-               },
-               {
-                model:serviceModel,
-                as:'dealer_history',
-                attributes:['rating','comments'],
-                order: [["created_at", "DESC"]],
-                limit:3
-              }
-           ]
-            })
-             res.json(dealersResult)
-      }
-      catch (err) {
-       if (!err.statusCode) {
-         err.statusCode = 500;
-       }
-       next(err);
-     }
-   };
+		let dealersResult = await dealerModel.findAll({
+			where: { city: cityValue },
+			include: [
+				{
+					model: vehicleModel,
+					as: 'Vehicletype',
+					attributes: ['vehicle_type'],
+					where: { id: vehicleId }
+				},
+				{
+					model: dealerServices,
+					as: 'Services',
+					attributes: ['discription', 'cost']
+				},
+				{
+					model: serviceModel,
+					as: 'dealer_history',
+					attributes: ['rating', 'comments'],
+					order: [['created_at', 'DESC']],
+					limit: 3
+				}
+			]
+		});
+		res.json(dealersResult);
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
 
-   exports.getServiceByDealerID = async (req, res, next) => {
-    try{
-        var id = req.params.id;
-          let servicesResult = await dealerServices.findAll(
-             {
-              attributes:['service_id','discription'],
-              where :{dealerTblDealerId: id},
-              include:[
-                {
-                 model: serviceTypes,
-                 as :'serviceTypes' ,
-                 attributes:['service_type','service_name']
-                }
-           ]
-            })
-             res.json(servicesResult)
-      }
-      catch (err) {
-       if (!err.statusCode) {
-         err.statusCode = 500;
-       }
-       next(err);
-     }
-   };
+exports.getServiceByDealerID = async (req, res, next) => {
+	try {
+		var id = req.params.id;
+		let servicesResult = await dealerServices.findAll({
+			attributes: ['service_id', 'discription'],
+			where: { dealerTblDealerId: id },
+			include: [
+				{
+					model: serviceTypes,
+					as: 'serviceTypes',
+					attributes: ['service_type', 'service_name']
+				}
+			]
+		});
+		res.json(servicesResult);
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
 
-  exports.AddDealer = async (req, res, next) => {
-    try{
-          const result = await dealerModel.create(req.body);  
-          const resultID=result.dealer_id;
-          var serviceData = req.body.services.map(function(item) {
-            var updatedService = Object.assign({}, item);
-            updatedService.dealerTblDealerId =resultID
-            return updatedService;
-          })
-          await dealerServices.bulkCreate(serviceData)      
-          res.json(result);
-    }
-    catch(err) {
-        if (!err.statusCode) {
-          err.statusCode = 500;
-        }
-        next(err);
- }
-  };
+exports.AddDealer = async (req, res, next) => {
+	try {
+		const result = await dealerModel.create(req.body);
+		const resultID = result.dealer_id;
+		var serviceData = req.body.services.map(function (item) {
+			var updatedService = Object.assign({}, item);
+			updatedService.dealerTblDealerId = resultID;
+			return updatedService;
+		});
+		await dealerServices.bulkCreate(serviceData);
+		res.json(result);
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
+
+//Check dealer credentials based on username & password entered
+exports.checkDealerCredentials = async (req, res, next) => {
+	try {
+		var email = req.body.email;
+		var password = req.body.password;
+
+		let dealerData = await dealerModel.findOne({
+			attributes: ['dealer_id'],
+			where: {
+				email,
+				password
+			}
+		});
+		if (dealerData) {
+			res.send(dealerData);
+		} else {
+			res.send({ message: 'Wrong email/password combination! Please try again' });
+		}
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	}
+};
